9dde9e0d3cbcde7394cd1717014198fcfcdbc601
Saif Momani <saif.aq.momani@gmail.com>
2021-03-28 14:48:59 +0300

    lab5

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,18 @@
 function changeColor() {
-  var color1 = document.body.style.backgroundColor;
-  var color2 = prompt("Please Choose a Color", "white");
+  var type = prompt("Do you like cats or dogs?");
+  while (type !== "cats" && type !== "dogs")
+    type = prompt("please choose either cats or dogs");
 
-  if (color2 != null) {
-    document.body.style.backgroundColor = color2;
-    if (
-      confirm(
-        "the background is now " +
-          color2 +
-          " click cancel to revert back to original color"
-      ) === false
-    )
-      document.body.style.backgroundColor = color1;
+  var number = prompt("How many do you want?", 1);
+
+  if (number != null) {
+    for (i = 0; i < number; i++) {
+      if (type === "dogs")
+        document.getElementById("img").innerHTML +=
+          '<img src="https://www.sathguru.com/wp-content/uploads/2020/07/A-Market-assessment-of-Animal-Healthcare-Industry-Landscape-of-Vaccines-and-Diagnostics-Sector-in-India-1280x671.jpg"/>';
+      if (type === "cats")
+        document.getElementById("img").innerHTML +=
+          '<img src="https://www.aspca.org/sites/default/files/cat-care_general-cat-care_body1-right.jpg"/>';
+    }
   }
 }
